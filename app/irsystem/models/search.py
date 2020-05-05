46e00e1ddab4f0a5dacb0bb10a54bcdc4acdf543
Collin Montag <cm759@cornell.edu>
2020-05-04 21:36:22 -0400

    exclude self from get similar search

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -23,7 +23,7 @@ def score(dist):
 
 def search_drinks(drinks, args):
     query = None
-    count = len(drinks)
+    drink_name = ''
 
     # Fetch query vector from drink name
     if type(args.data) == str:
@@ -31,6 +31,7 @@ def search_drinks(drinks, args):
         if vbytes is None:
             return []
         query = np.array(json.loads(vbytes))
+        drink_name = args.data
     # Form query vector from word embeddings
     if type(args.data) == list:
         emb_dict = {e.word: np.array(json.loads(e.vbytes)) for e in query_embeddings()}
@@ -41,8 +42,8 @@ def search_drinks(drinks, args):
         return [(d, None) for d in drinks]
 
     # Search database results for k nearest neighbors
-    d_vectors = [np.array(json.loads(d.vbytes)) for d in drinks]
-    knn_data = np.array(d_vectors).reshape(count, -1)
+    d_vectors = [np.array(json.loads(d.vbytes)) for d in drinks if d.name != drink_name]
+    knn_data = np.array(d_vectors).reshape(len(d_vectors), -1)
     dst_vec = cdist([query], knn_data, 'cosine')[0]
     ind_vec = np.argsort(dst_vec)
     
